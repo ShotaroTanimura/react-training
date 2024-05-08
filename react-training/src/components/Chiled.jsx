3cf1f8358cc3c74c0cf222116d8b1b093c24b862
Shotaro Tanimura <144506009+ShotaroTanimura@users.noreply.github.com>
2024-05-08 22:17:53 +0900

    2024-05-08

---
+++ react-training/src/components/Chiled.jsx
@@ -0,0 +1,11 @@
+import "./Child.css"
+
+const practice = () => {
+  return (
+  <div className="header">  
+  <h1>Header</h1>  
+  <p>My supercool header</p>  
+</div>
+  )
+}
+export default practice
